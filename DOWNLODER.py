76db5473069f8bfa3761d8c3f8d0b30d30cc179d
Fredy Somy <65971072+fredysomy@users.noreply.github.com>
2020-06-05 19:05:16 +0530

    download song ENABLED,, got to add GUI

--- DOWNLODER.py
+++ DOWNLODER.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+import time
 
 a=input("name")
 
@@ -15,7 +16,18 @@ WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.ID, "vid
 links = driver.find_elements_by_id("video-title")
 
 a=links[0].get_attribute('href')
-print(a)
+
+driver.get('https://ytmp3.cc/en13/')
+driver.find_element_by_id('input').send_keys(a)
+driver.find_element_by_id('submit').click()
+
+time.sleep(3)
+a=driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/div[3]/a[1]')
+song=a.get_attribute('href')
+driver.get(song)
+
+
+
 
 
 
